6a7b82a177e482675af86cfb3f18a077b194719b
Drew Bredvick <drew.bredvick@spreetail.com>
2019-05-22 12:46:44 -0500

    trying again

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import Header from "../components/header";
 import Menu from '../components/menu';
 import Hero from '../components/hero';
 import { FaTools } from 'react-icons/fa';
 
 
-function Index() {
+const Index = () => {
   return (
     <>
       <main>
@@ -13,6 +12,9 @@ function Index() {
         <Hero />
         <section>
           <p><FaTools /> This site is currently under construction. Come back later for great content. <FaTools /></p>
+        </section>
+        <section>
+          <div id="instafeed"></div>
         </section>
       </main>
       <style jsx global>{`
